5c099b916f2abc4f1eccf5fa45fbefef40b7ece6
Chris <christianamancio2011@hotmail.com>
2026-04-03 17:44:36 -0300

    Implementação do Firebase
    correção de erros e implementação do firebase

--- app/src/main/java/br/com/ifsp/gidessu/microredesocial/HomeActivity.kt
+++ app/src/main/java/br/com/ifsp/gidessu/microredesocial/HomeActivity.kt
@@ -1,13 +1,21 @@
 package br.com.ifsp.gidessu.microredesocial
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.scale
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.com.ifsp.gidessu.microredesocial.databinding.ActivityHomeBinding
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
+import java.io.ByteArrayOutputStream
+import kotlin.io.encoding.Base64
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -27,7 +35,26 @@ class HomeActivity : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
+
+
+
+        val firebaseAuth = FirebaseAuth.getInstance()
+        val email = firebaseAuth.currentUser!!.email.toString()
+        val db = Firebase.firestore
+        db.collection("usuarios").document(email).get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful){
+                    val document = task.result
+                    val imageString = document.data!!["fotoPerfil"].toString()
+                    val bitmap = Base64Converter.stringToBitmap(imageString)
+                    binding.imgLogo.setImageBitmap(bitmap)
+                    binding.txtUsername.text = document.data!!["username"].toString()
+                    binding.txtNomeCompleto.text = document.data!!["nomeCompleto"].toString()
+                }
+            }
     }
+
+
 
 
 
--- app/src/main/java/br/com/ifsp/gidessu/microredesocial/ProfileActivity.kt
+++ app/src/main/java/br/com/ifsp/gidessu/microredesocial/ProfileActivity.kt
@@ -1,5 +1,6 @@
 package br.com.ifsp.gidessu.microredesocial
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -9,6 +10,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.com.ifsp.gidessu.microredesocial.databinding.ActivityProfileBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
@@ -21,6 +25,8 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         changeProfile()
+
+        saveProfile()
 
     }
 
@@ -42,6 +48,29 @@ class ProfileActivity : AppCompatActivity() {
         }
     }
 
+    fun saveProfile() {
+        binding.btnEditPhoto.setOnClickListener {
+            val firebaseAuth = FirebaseAuth.getInstance()
+            if (firebaseAuth.currentUser != null) {
+                val email = firebaseAuth.currentUser!!.email.toString()
+                val username = binding.username.text.toString()
+                val nomeCompleto = binding.nomeCompleto.text.toString()
+                val fotoPerfilString = Base64Converter.drawableToString(binding.edtProfile.drawable)
+                val db = Firebase.firestore
+                val dados = hashMapOf(
+                    "nomeCompleto" to nomeCompleto,
+                    "username" to username,
+                    "fotoPerfil" to fotoPerfilString
+                )
+                db.collection("usuarios").document(email)
+                    .set(dados)
+                    .addOnSuccessListener {
+                        startActivity(Intent(this, HomeActivity::class.java))
+                        finish()
+                    }
+            }
+        }
+    }
 
 
 
